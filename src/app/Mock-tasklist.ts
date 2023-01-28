1c90ac6e17aaa6f57b7d4d35a9220b2fc6093204
Cristian <krhyz02@gmail.com>
2023-01-28 14:14:15 -0300

    v1.0

---
+++ src/app/Mock-tasklist.ts
@@ -0,0 +1,24 @@
+import {Task} from "./Task"
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'terminar primer modulo de angular',
+    day: 'Agosto 5 a las 12:00',
+    reminder: true
+  },
+  { id: 2,
+     text: 'hacer compras',
+      day: 'Agosto 5 a las 15:00',
+       reminder: true 
+      },
+  {
+    id: 3,
+    text: 'investigar boostsrap',
+    day: 'Agosto 5 a las 19:00',
+    reminder: false
+  },
+  { id: 4, 
+     text: 'Leer libros',
+     day: 'Agosto 5 a las 21:00', 
+     reminder: false }
+];
